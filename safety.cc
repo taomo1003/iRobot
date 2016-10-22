b40db03e41f3652342cac279f3613378c5ac7b07
Tao Mo <taomo1003@gmail.com>
2016-10-22 16:35:06 -0500

    Oct22.3

--- safety.cc
+++ safety.cc
@@ -10,7 +10,13 @@ void* safety(void* parms)
 	}
 	
 	bool mtxLocked = false;
+	bool overcurrnt = false;
 	
+	Create::note_t note1(60,45);
+	Create::note_t note2(Create::NO_NOTE,70);
+	Create::song_t song = {note1, note2};
+	robot->sendSongCommand(0,song);
+				
 	while(true)
 	{
 		//if a safety sencer is fired stop the robot.
@@ -19,6 +25,10 @@ void* safety(void* parms)
 		   robot->cliffLeftSignal()		  || robot->cliffFrontLeftSignal()	||
 		   robot->cliffRightSignal()	  || robot->cliffFrontRightSignal()									)
 		{
+			if(robot->rightWheelOvercurrent() || robot->leftWheelOvercurrent())
+			{
+				overcurrnt=true;
+			}
 			if (!mtxLocked)
 			{
 				cout << "Safety Locked." << endl;
@@ -26,12 +36,17 @@ void* safety(void* parms)
 				mtxLocked = true;
 				robot->sendDriveCommand(0.0, Create::DRIVE_STRAIGHT);
 			}
-			
+			robot->sendPlaySongCommand(0);
+		}
+		
+		if(robot->playButton())
+		{
+			overcurrnt=false;
 		}
 		
 		//if no saftey sencers are fired start the robot
 		if(!robot->wheeldropLeft()         && !robot->wheeldropRight()        && !robot->wheeldropCaster() &&
-		   !robot->rightWheelOvercurrent() && !robot->leftWheelOvercurrent()  &&
+		   !overcurrnt					   &&
 		   !robot->cliffLeftSignal()	   && !robot->cliffFrontLeftSignal()  &&
 		   !robot->cliffRightSignal()	   && !robot->cliffFrontRightSignal()									)
 		{
